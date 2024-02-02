cf1265958165ef659e8a4b0f3eef16429329e6f1
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-02-02 12:00:52 +0630

    updated abc

--- resources/views/admin/two_d/two_d_evening_play_index.blade.php
+++ resources/views/admin/two_d/two_d_evening_play_index.blade.php
@@ -26,7 +26,7 @@
                         </div>
                         <div class="ms-auto my-auto mt-lg-0 mt-4">
                             <div class="ms-auto my-auto">
-                                <a href="{{ route('admin.users.create') }}"
+                                <a href="{{ url('/admin/twod-daily-evening-history') }}"
                                     class="btn bg-gradient-primary btn-sm mb-0"> > &nbsp; 4:30 စာရင်း ပေါင်းချုပ် </a>
                                 <button class="btn btn-outline-primary btn-sm export mb-0 mt-sm-0 mt-1" data-type="csv"
                                     type="button" name="button">Export</button>
