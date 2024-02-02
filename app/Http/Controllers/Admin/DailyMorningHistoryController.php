059500ab05d4a9cd291d79e3715b9430faca7b61
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-02-02 15:38:53 +0630

    update

--- app/Http/Controllers/Admin/DailyMorningHistoryController.php
+++ app/Http/Controllers/Admin/DailyMorningHistoryController.php
@@ -4,28 +4,48 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Models\Admin\Lottery;
 use App\Models\Admin\TwoDLimit;
+use App\Http\Controllers\Controller;
 
 class DailyMorningHistoryController extends Controller
 {
-   public function TwodDailyMorningHistory()
-{
-    $displayTwoDigits = User::getAdmin2dDailyMorningHistory();
-    $twod_limits = TwoDLimit::orderBy('id', 'desc')->first();
-    return view('admin.two_d.dailymorning_history', [
-        'displayTwoDigits' => $displayTwoDigits,
-        'twod_limits' => $twod_limits,
-    ]);
-}
+    public function TwodDailyMorningHistory()
+    {
+        $lotteries = (new Lottery)->getLotteriesWithUserAndTwoDigits('5:00', '12:30');
+        $twod_limits = TwoDLimit::orderBy('id', 'desc')->first();
+        return view('admin.two_d.dailymorning_history', [
+            'displayTwoDigits' => $lotteries,
+            'twod_limits' => $twod_limits,
+        ]);
+    }
 
-public function TwodDailyEveningHistory()
-{
-    $displayTwoDigits = User::getAdmin2dDailyEveningHistory();
-    $twod_limits = TwoDLimit::orderBy('id', 'desc')->first();
-    return view('admin.two_d.dailyeveing_history', [
-        'displayTwoDigits' => $displayTwoDigits,
-        'twod_limits' => $twod_limits,
-    ]);
+    public function TwodDailyEveningHistory()
+    {
+        $displayTwoDigits = User::getAdmin2dDailyEveningHistory();
+        $twod_limits = TwoDLimit::orderBy('id', 'desc')->first();
+        return view('admin.two_d.dailyeveing_history', [
+            'displayTwoDigits' => $displayTwoDigits,
+            'twod_limits' => $twod_limits,
+        ]);
+    }
 }
-}
+//    public function TwodDailyMorningHistory()
+// {
+//     $displayTwoDigits = User::getAdmin2dDailyMorningHistory();
+//     $twod_limits = TwoDLimit::orderBy('id', 'desc')->first();
+//     return view('admin.two_d.dailymorning_history', [
+//         'displayTwoDigits' => $displayTwoDigits,
+//         'twod_limits' => $twod_limits,
+//     ]);
+// }
+
+// public function TwodDailyEveningHistory()
+// {
+//     $displayTwoDigits = User::getAdmin2dDailyEveningHistory();
+//     $twod_limits = TwoDLimit::orderBy('id', 'desc')->first();
+//     return view('admin.two_d.dailyeveing_history', [
+//         'displayTwoDigits' => $displayTwoDigits,
+//         'twod_limits' => $twod_limits,
+//     ]);
+// }
