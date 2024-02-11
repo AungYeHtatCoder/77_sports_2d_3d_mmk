df0cae346817a39406aad5fb74ba90ccf5ace4a9
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-02-11 12:21:31 +0630

    updated time

--- app/Services/TwoDLotteryService.php
+++ app/Services/TwoDLotteryService.php
@@ -11,35 +11,41 @@ class TwoDLotteryService
     // Retrieve two digits for a user based on session ('morning' or 'evening')
     public function getUserTwoDigits($userId, $session)
     {
-        $sessionTimes = $this->getSessionTimes($session);
+     $sessionTimes = $this->getSessionTimes($session);
 
-        $twoDigits = Lottery::where('user_id', $userId)
-                            ->with(['twoDigitsForSession' => function ($query) use ($sessionTimes) {
-                                // Assuming 'twoDigitsForSession' is correctly defined in the Lottery model
-                                // to filter two digits based on the session time
-                                $query->wherePivotBetween('created_at', [$sessionTimes['start'], $sessionTimes['end']]);
-                            }])
-                            ->get()
-                            ->pluck('twoDigitsForSession')
-                            ->collapse()
-                            ->map(function ($twoDigit) {
-                                // Convert main model timestamps
-                                $twoDigit->created_at = Carbon::parse($twoDigit->created_at)->timezone('Asia/Yangon')->toDateTimeString();
-                                $twoDigit->updated_at = Carbon::parse($twoDigit->updated_at)->timezone('Asia/Yangon')->toDateTimeString();
-                                
-                                // Convert pivot timestamps
-                                $twoDigit->pivot->created_at = Carbon::parse($twoDigit->pivot->created_at)->timezone('Asia/Yangon')->toDateTimeString();
-                                $twoDigit->pivot->updated_at = Carbon::parse($twoDigit->pivot->updated_at)->timezone('Asia/Yangon')->toDateTimeString();
-                                
-                                return $twoDigit;
-                            });
+     $twoDigits = Lottery::where('user_id', $userId)
+          ->with(['twoDigitsForSession' => function ($query) use ($sessionTimes) {
+           $query->wherePivotBetween('created_at', [$sessionTimes['start'], $sessionTimes['end']]);
+          }])
+          ->get()
+          ->pluck('twoDigitsForSession')
+          ->collapse()
+          ->map(function ($twoDigit) {
+           // Convert main model timestamps
+           if ($twoDigit->created_at && $twoDigit->created_at instanceof \DateTime) {
+            $twoDigit->created_at = Carbon::parse($twoDigit->created_at)->timezone('Asia/Yangon')->toDateTimeString();
+           }
+           if ($twoDigit->updated_at && $twoDigit->updated_at instanceof \DateTime) {
+            $twoDigit->updated_at = Carbon::parse($twoDigit->updated_at)->timezone('Asia/Yangon')->toDateTimeString();
+           }
+           
+           // Convert pivot timestamps
+           if ($twoDigit->pivot->created_at && $twoDigit->pivot->created_at instanceof \DateTime) {
+            $twoDigit->pivot->created_at = Carbon::parse($twoDigit->pivot->created_at)->timezone('Asia/Yangon')->toDateTimeString();
+           }
+           if ($twoDigit->pivot->updated_at && $twoDigit->pivot->updated_at instanceof \DateTime) {
+            $twoDigit->pivot->updated_at = Carbon::parse($twoDigit->pivot->updated_at)->timezone('Asia/Yangon')->toDateTimeString();
+           }
+           
+           return $twoDigit;
+          });
 
-        $totalAmount = $twoDigits->sum('pivot.sub_amount');
+     $totalAmount = $twoDigits->sum('pivot.sub_amount');
 
-        return [
-            'two_digits' => $twoDigits,
-            'total_amount' => $totalAmount
-        ];
+     return [
+      'two_digits' => $twoDigits,
+      'total_amount' => $totalAmount
+     ];
     }
 
     // Define session times based on session name
