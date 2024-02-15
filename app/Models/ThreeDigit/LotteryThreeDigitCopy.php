797d4293f678785cdbba11eb10212bcb8754c21d
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-02-16 01:32:12 +0630

    3d play

--- app/Models/ThreeDigit/LotteryThreeDigitCopy.php
+++ app/Models/ThreeDigit/LotteryThreeDigitCopy.php
@@ -11,7 +11,4 @@ class LotteryThreeDigitCopy extends Model
     use HasFactory;
     protected $table = 'lotto_three_digit_copy';
     protected $fillable = ['three_digit_id', 'lotto_id', 'sub_amount', 'prize_sent'];
-
-    
-
 }
