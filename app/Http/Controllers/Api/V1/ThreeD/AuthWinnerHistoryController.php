a7176c12295b92c8530976685d0f5f5d602738cd
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-03-27 08:54:19 +0630

    prize_winners updated by amk

--- app/Http/Controllers/Api/V1/ThreeD/AuthWinnerHistoryController.php
+++ app/Http/Controllers/Api/V1/ThreeD/AuthWinnerHistoryController.php
@@ -92,12 +92,57 @@ class AuthWinnerHistoryController extends Controller
         $this->updatePermutationPrizeSentDate($permutation->user_id); // Make sure user_id is the ID of the winner
     }
 
+    // greaters winners
+    $oneMAgo = Carbon::now()->subMonth();
+    $prize_winners = DB::table('lotto_three_digit_copy')
+        ->join('three_digits', 'lotto_three_digit_copy.three_digit_id', '=', 'three_digits.id')
+        ->join('lottos', 'lotto_three_digit_copy.lotto_id', '=', 'lottos.id')
+        ->join('users', 'lottos.user_id', '=', 'users.id')
+        ->where('lottos.user_id', $userId)
+        ->join('prizes', function($join) {
+            $join->on('three_digits.three_digit', '=', 'prizes.prize_one')
+                 ->orOn('three_digits.three_digit', '=', 'prizes.prize_two');
+        })
+        ->whereDate('prizes.created_at', '>=', $oneMAgo)
+        ->groupBy(
+            'lottos.user_id', 
+            'users.name',
+            'users.profile',
+            'users.phone',
+            'lotto_three_digit_copy.sub_amount', 
+            'lotto_three_digit_copy.prize_sent',
+            'lottos.total_amount', 
+            'prizes.prize_one', 
+            'prizes.prize_two', 
+            'prizes.created_at',  
+        )
+        ->select(
+            'lottos.user_id', 
+            'users.name',
+            'users.profile',
+            'users.phone',
+            'lotto_three_digit_copy.sub_amount',
+            'lotto_three_digit_copy.prize_sent',
+            'lottos.total_amount',
+            'prizes.prize_one', 
+            'prizes.prize_two', 
+            'prizes.created_at', 
+            DB::raw('lotto_three_digit_copy.sub_amount * 10 as prize_amount')
+        )
+        ->orderBy('prize_amount', 'desc') // Add this line to sort by prize_amount in descending order
+        ->get();
+
+    // Update the prize_sent date for each winner
+    foreach ($prize_winners as $winner) {
+        $this->updateGreatePrizeSentDate($winner->user_id); // Make sure user_id is the ID of the winner
+    }
 
      return response()->json([
             'success' => true,
             'message' => 'Data fetched successfully',
             'winners' => $winners,
             'permutation_winners' => $permutation_winners,
+            'prize_winners' => $prize_winners,
         ], 200);
     }catch (\Exception $e) {
         return response()->json([
@@ -145,5 +190,25 @@ class AuthWinnerHistoryController extends Controller
 
         return redirect()->back()->with('success', 'Prize sent date updated successfully!');
     }
+
+    public function updateGreatePrizeSentDate($winnerId)
+    {
+        // Find the lottery_two_digit_pivot record
+        $lotteryTwoDigitPivot = DB::table('lotto_three_digit_copy')->where('lotto_id', $winnerId)->first();
+
+        // Check if the record exists
+        if (!$lotteryTwoDigitPivot) {
+            return redirect()->back()->with('error', 'Record not found!');
+        }
+
+        // Update the prize_sent field to true
+        DB::table('lotto_three_digit_copy')
+            ->where('lotto_id', $winnerId)
+            ->update(['prize_sent' => 3]);
+
+        return redirect()->back()->with('success', 'Prize sent date updated successfully!');
+    }
+
+
 
 }
